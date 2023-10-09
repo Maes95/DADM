0307ced3575338c3835d73c21d7c6e883918923a
Maes95 <michel.maes95@gmail.com>
2023-10-09 18:24:27 +0200

    Ejercicio_2-Checkpoint_1

---
+++ Ejemplo_2/app/src/main/java/dev/maes/ejemplo_2/WellnessTask.kt
@@ -0,0 +1,3 @@
+package dev.maes.ejemplo_2
+
+data class WellnessTask(val id: Int, val label: String)
---
+++ Ejemplo_2/app/src/main/java/dev/maes/ejemplo_2/WellnessTaskItem.kt
@@ -0,0 +1,69 @@
+package dev.maes.ejemplo_2
+
+import androidx.compose.foundation.layout.Row
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
+import androidx.compose.material3.Checkbox
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.tooling.preview.Preview
+import dev.maes.ejemplo_2.ui.theme.Ejemplo_2Theme
+
+@Composable
+fun WellnessTaskItem(
+    taskName: String,
+    checked: Boolean,
+    onCheckedChange: (Boolean) -> Unit,
+    onClose: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Row(
+        modifier = modifier, verticalAlignment = Alignment.CenterVertically
+    ) {
+        Text(
+            modifier = Modifier
+                .weight(1f)
+                .padding(start = 16.dp),
+            text = taskName
+        )
+        Checkbox(
+            checked = checked,
+            onCheckedChange = onCheckedChange
+        )
+        IconButton(onClick = onClose) {
+            Icon(Icons.Filled.Close, contentDescription = "Close")
+        }
+    }
+}
+
+@Composable
+fun WellnessTaskItem(taskName: String, modifier: Modifier = Modifier) {
+    var checkedState by remember { mutableStateOf(false) }
+
+    WellnessTaskItem(
+        taskName = taskName,
+        checked = checkedState,
+        onCheckedChange = { newValue -> checkedState = newValue },
+        onClose = {}, // we will implement this later!
+        modifier = modifier,
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun WellnessTaskItem_example(){
+    Ejemplo_2Theme {
+        WellnessTaskItem("Task 1")
+    }
+}
+
