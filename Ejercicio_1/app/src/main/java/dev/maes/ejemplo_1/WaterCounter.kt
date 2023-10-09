b18498211e35bc916cf9eec81b7a1b1d44898e2c
Maes95 <michel.maes95@gmail.com>
2023-10-09 17:41:51 +0200

    Ejercicio_1-Checkpoint_3_Solution

--- Ejercicio_1/app/src/main/java/dev/maes/ejemplo_1/WaterCounter.kt
+++ Ejercicio_1/app/src/main/java/dev/maes/ejemplo_1/WaterCounter.kt
@@ -18,7 +18,7 @@ fun WaterCounter(modifier: Modifier = Modifier) {
         if (count > 0) {
             Text("You've had $count glasses.")
         }
-        Button(onClick = { count++ }, Modifier.padding(top = 8.dp)) {
+        Button(onClick = { count++ }, Modifier.padding(top = 8.dp), enabled = count < 10) {
             Text("Add one")
         }
     }
