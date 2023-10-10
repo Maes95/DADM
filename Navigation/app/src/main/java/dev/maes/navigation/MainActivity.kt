b82f3552f0552b39cbdde14cc7b4241c79552558
Maes95 <michel.maes95@gmail.com>
2023-10-11 00:55:02 +0200

    Ejercicio_Navigation-Checkpoint_1

--- Navigation/app/src/main/java/dev/maes/navigation/MainActivity.kt
+++ Navigation/app/src/main/java/dev/maes/navigation/MainActivity.kt
@@ -3,17 +3,17 @@ package dev.maes.navigation
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
 import dev.maes.navigation.ui.components.CustomTabRow
 import dev.maes.navigation.ui.theme.NavigationTheme
 
@@ -31,21 +31,33 @@ class MainActivity : ComponentActivity() {
 fun App() {
     NavigationTheme {
 
-        var currentScreen:Destination by remember { mutableStateOf(Android) }
+        val navController = rememberNavController()
+        val currentBackStack by navController.currentBackStackEntryAsState()
+        val currentDestination = currentBackStack?.destination
+        val currentScreen = tabRowScreens.find { it.route == currentDestination?.route } ?: Android
 
         Scaffold(
             topBar = {
                 CustomTabRow(
                     allScreens = tabRowScreens,
                     onTabSelected = { newScreen ->
-                        currentScreen = newScreen
+                        navController.navigate(newScreen.route){ launchSingleTop = true }
                     },
                     currentScreen = currentScreen
                 )
             }
         ) { innerPadding ->
-            Box(Modifier.padding(innerPadding)) {
-                currentScreen.screen()
+            NavHost(
+                navController = navController,
+                startDestination = Android.route,
+                modifier = Modifier.padding(innerPadding)
+            ) {
+                composable(route = Android.route) {
+                    Android.screen()
+                }
+                composable(route = Courses.route) {
+                    Courses.screen()
+                }
             }
         }
     }
