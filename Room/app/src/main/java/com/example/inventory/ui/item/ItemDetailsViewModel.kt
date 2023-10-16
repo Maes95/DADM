868c818979352b4111b4ce8fcecd688fc766e551
Maes95 <michel.maes95@gmail.com>
2023-10-16 17:56:44 +0200

    Ejercicio_Room_Checkpoint_4

--- Room/app/src/main/java/com/example/inventory/ui/item/ItemDetailsViewModel.kt
+++ Room/app/src/main/java/com/example/inventory/ui/item/ItemDetailsViewModel.kt
@@ -46,6 +46,11 @@ class ItemDetailsViewModel(
                 started = SharingStarted.WhileSubscribed(TIMEOUT_MILLIS),
                 initialValue = ItemDetailsUiState()
             )
+
+    suspend fun deleteItem() {
+        itemsRepository.deleteItem(uiState.value.itemDetails.toItem())
+    }
+
     companion object {
         private const val TIMEOUT_MILLIS = 5_000L
     }
