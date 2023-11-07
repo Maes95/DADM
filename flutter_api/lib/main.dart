789e1cee986d3b87a177bf72d988bc5594d26e40
Maes95 <michel.maes95@gmail.com>
2023-11-08 00:09:29 +0100

    Exercise 1 - Solution

--- flutter_api/lib/main.dart
+++ flutter_api/lib/main.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:http/http.dart' as http;
 import 'Character.dart';
 
@@ -38,7 +39,7 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Rick and Morty Characters App',
+      title: 'Fetch Data Example',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
@@ -46,23 +47,64 @@ class _MyAppState extends State<MyApp> {
         appBar: AppBar(
           title: const Text('Rick and Morty Characters'),
         ),
-        body: FutureBuilder<List<Character>>(
-          future: charactersFuture,
-          builder: (context, snapshot) {
-            if (snapshot.hasData) {
-              return ListView.builder(
-                itemCount: snapshot.data!.length,
-                itemBuilder: (context, index) {
-                  Character character = snapshot.data![index];
-                  return Text(character.name);
-                },
-              );
-            } else if (snapshot.hasError) {
-              return Text('${snapshot.error}');
-            }
-            // By default, show a loading spinner.
-            return const CircularProgressIndicator();
-          },
+        body: Center(
+          child: FutureBuilder<List<Character>>(
+            future: charactersFuture,
+            builder: (context, snapshot) {
+              if (snapshot.hasData) {
+                return ListView.builder(
+                  itemCount: snapshot.data!.length,
+                  itemBuilder: (context, index) {
+                    Character character = snapshot.data![index];
+                    return CharacterWidget(character: character);
+                  },
+                );
+              } else if (snapshot.hasError) {
+                return Text('${snapshot.error}');
+              }
+              // By default, show a loading spinner.
+              return const CircularProgressIndicator();
+            },
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class CharacterWidget extends StatelessWidget {
+  const CharacterWidget({
+    super.key,
+    required this.character,
+  });
+
+  final Character character;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(20),
+      child: Card(
+        child: Column(
+          children: <Widget>[
+            SizedBox(
+              height: 300,
+              child: Ink.image(
+                image: Image.network(character.image).image,
+                fit: BoxFit.fitWidth,
+              ),
+            ),
+            Center(
+              child: Padding(
+                padding: const EdgeInsets.all(25.0),
+                child: Text(
+                  character.name,
+                  style: const TextStyle(fontFamily: 'RickAndMorty', fontSize: 40, color: Colors.cyan),
+                  //style: GoogleFonts.pacifico(fontSize: 40, color: Colors.black),
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );
