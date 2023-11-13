61ba59b62970f8b8098e139b2857dc95a9ffcecb
Maes95 <michel.maes95@gmail.com>
2023-11-13 20:17:42 +0100

    Flutter DB example: Create Character

--- flutter_bbdd/lib/main.dart
+++ flutter_bbdd/lib/main.dart
@@ -29,7 +29,18 @@ class _HomeScreenState extends State<HomeScreen> {
       characters = data;
     });
   }
-  // No copiar
+
+  final TextEditingController _nameController = TextEditingController();
+  final TextEditingController _imageController = TextEditingController();
+
+  Future<void> addCharacter() async {
+    await CharacterService.insertCharacter(
+        Character(name: _nameController.text, image: _imageController.text));
+    _nameController.clear();
+    _imageController.clear();
+    _getRefreshCharacters();
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -44,8 +55,16 @@ class _HomeScreenState extends State<HomeScreen> {
         body: Center(
           child: Column(
             children: [
+              CharacterForm(
+                  nameController: _nameController,
+                  imageController: _imageController,
+                  onCreateCharacter: (){
+                    setState(() {
+                      addCharacter();
+                    });
+                  }
+              ),
               Expanded(
-                  // No copiar
                   child: ListView(
                 children: [
                   for (var character in characters)
@@ -57,6 +76,50 @@ class _HomeScreenState extends State<HomeScreen> {
             ],
           ),
         ),
+      ),
+    );
+  }
+}
+
+class CharacterForm extends StatelessWidget {
+  const CharacterForm({
+    super.key,
+    required TextEditingController nameController,
+    required TextEditingController imageController,
+    required this.onCreateCharacter,
+  }) : _nameController = nameController, _imageController = imageController;
+
+  final TextEditingController _nameController;
+  final TextEditingController _imageController;
+  final Function onCreateCharacter;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(15.0),
+      child: Column(
+        children: [
+          TextField(
+            controller: _nameController,
+            decoration: const InputDecoration(hintText: 'Name'),
+          ),
+          const SizedBox(
+            height: 10,
+          ),
+          TextField(
+            controller: _imageController,
+            decoration: const InputDecoration(hintText: 'Image'),
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          ElevatedButton(
+            onPressed: (){
+              onCreateCharacter();
+            },
+            child: const Text('Create new character'),
+          )
+        ],
       ),
     );
   }
